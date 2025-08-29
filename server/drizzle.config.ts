9c6293ec3e061b8397f48634cce22e989ce8a86d
Ash <105349273+Ashfaith@users.noreply.github.com>
2025-08-29 14:07:26 +1200

    changing drizzle config for ca cert

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,14 +1,23 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-const caCert = process.env.CA_CERT?.replace(/\\n/g, "\n");
+const caCert = process.env.DATABASE_CA_CERT?.replace(/\\n/g, "\n");
+
+console.log(
+  "CA cert loaded?",
+  caCert?.startsWith("-----BEGIN CERTIFICATE-----")
+);
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASS!,
+    database: process.env.DATABASE_NAME!,
     ssl: {
       rejectUnauthorized: true,
       ca: caCert,
