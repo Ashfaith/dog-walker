8cda6239fa154fc8038b63bdca658e96780da110
Ash <105349273+Ashfaith@users.noreply.github.com>
2025-08-30 13:28:35 +1200

    changing pool to manualy set database connection instead of connectionString

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -7,7 +7,11 @@ console.log("CA cert loaded?", !!process.env.DATABASE_CA_CERT);
 
 const poolConfig = isProduction
   ? {
-      connectionString: process.env.DATABASE_URL,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
       ssl: {
         rejectUnauthorized: true,
         ca: process.env.DATABASE_CA_CERT?.replace(/\\n/g, "\n"),
